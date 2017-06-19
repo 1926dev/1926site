a80f0fb16373bcd29bc7d635ce61fa9ca56f1e69
Andrew Kolisz <andrew@1926marketing.com>
2017-06-19 16:25:58 -0400

    sassifying carousel border notch

--- assets/javascripts/carouselBorder.js
+++ assets/javascripts/carouselBorder.js
@@ -1,8 +1,24 @@
-$(init);
-
-function init() {
-	var notchColor = $("#myCarousel > .nav.pills > li.active:before").css("border-bottom-color");
-//	var selectParent = $("#myCarousel > .nav.pills > li.active:before").parentsUntil("#myCarousel");
-	
-	$("#myCarousel > .nav.pills > li.active:before").parentsUntil("#myCarousel").children(".carousel-inner").css("border-top-color", notchColor);
-}
+//$(init);
+//
+//function init() {
+//	//	var currentColor = $("#myCarousel > .nav-pills > li.active:before").css("border-color");
+//	//	$("#myCarousel > .nav-pills > li").hasClass(function (".active") {
+//	//		$(".nav-pills").css("border-bottom-color", currentColor);
+//	//	});
+//
+//	$('#myCarousel').on('borderListener', function () {
+//		var currentColor = $("#myCarousel .nav li.active:before").css("border-color");
+//		$("#myCarousel .nav li").css("border-color", currentColor);
+//	});
+//
+////	$someElement.on('event', function () {
+////		$('#myCarousel > .nav-pills > li.active:before').addClass('.active').trigger('classChange');
+////	});
+////
+////	// in another js file, far, far away
+////	$('#myCarousel > .nav-pills > li').on('classChange', function () {
+////		$("#myCarousel > .nav-pills > li").hasClass(function (".active") {
+////			$(".nav-pills").css("border-color", $("#myCarousel > .nav-pills > li.active:before").css("border-color"));
+////		});
+////	});
+//}
--- assets/javascripts/tabbedCarousel.js
+++ assets/javascripts/tabbedCarousel.js
@@ -7,15 +7,15 @@ $(document).ready(function () {
 	$('#myCarousel').on('click', '.nav a', function () {
 		clickEvent = true;
 		$('#myCarousel .nav li').removeClass('active');
-		$(this).parent().addClass('active');
+		$(this).parent().addClass('active').trigger('borderListener');
 	}).on('slid.bs.carousel', function (e) {
 		if (!clickEvent) {
 			var count = $('#myCarousel .nav').children().length - 1;
 			var current = $('#myCarousel .nav li.active');
-			current.removeClass('active').next().addClass('active');
+			current.removeClass('active').next().addClass('active').trigger('borderListener');
 			var id = parseInt(current.data('slide-to'));
 			if (count == id) {
-				$('#myCarousel .nav li').first().addClass('active');
+				$('#myCarousel .nav li').first().addClass('active').trigger('borderListener');
 			}
 		}
 		clickEvent = false;
